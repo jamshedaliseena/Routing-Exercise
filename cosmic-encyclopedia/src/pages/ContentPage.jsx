737cfc14c0d62212be87422b1230e45c7a84f990
jamshed <jamshedalisher321@gmail.com>
2026-06-24 16:51:40 -0700

    Routing Exercise
    Routing Exercise

---
+++ cosmic-encyclopedia/src/pages/ContentPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContentPage = ({data}) =>
+{
+	return (
+		<div>
+			<h1>{data.title}</h1>
+			<p>{data.content}</p>
+		</div>
+	);
+};
+
+export default ContentPage;
